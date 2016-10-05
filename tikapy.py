2fb6204c1b31b69e502a122c31497f53d289e990
Felix Hummel <felix@felixhummel.de>
2016-10-05 16:14:51 +0200

    initial

---
+++ tikapy.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+    >>> filename = 'example.pdf'
+    >>> get_metadata(filename)  # doctest: +ELLIPSIS
+    {...'title': 'Apache Tika - Wikipedia, the free encyclopedia'...}
+    >>> get_text(filename)  # doctest: +ELLIPSIS
+    'Apache Tika - Wikipedia...more than 1400 file types...MIME types...'
+"""
+import json
+import os
+import subprocess
+
+
+JAVA = os.environ.get('TIKA_JAVA', 'java')
+TIKA_JAR = os.environ.get('TIKA_JAR', 'tika-app-1.13.jar')
+
+
+def run(*args):
+    """
+    Run tika and capture both stderr and stdout.
+
+    Raises CalledProcessError on error. See
+    https://docs.python.org/3/library/subprocess.html#subprocess.run
+    """
+    cmd = [JAVA, '-jar', TIKA_JAR, '--encoding=utf-8'] + list(args)
+    return subprocess.run(
+            cmd,
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+
+
+def get_metadata(filename):
+    result = run('--json', filename)
+    unicode_stdout = result.stdout.decode('utf-8')
+    metadata = json.loads(unicode_stdout)
+    return metadata
+
+
+def get_text(filename):
+    result = run('--text-main', filename)
+    unicode_stdout = result.stdout.decode('utf-8')
+    return unicode_stdout
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
