c10833cbd02fb0254ddf2dcb056961f0113894e1
Felix Hummel <felix@felixhummel.de>
2016-10-05 16:27:32 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='tikapy',
+      version='1.0',
+      description='Minimal Wrapper around tika jar',
+      author='Felix Hummel',
+      author_email='felix@felixhummel.de',
+      py_modules=['tikapy'],
+     )
